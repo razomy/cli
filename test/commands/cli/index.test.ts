74dc7bfa98904ab148da35e18f0dfa24faf73345
Yevhenii Kamenskyi <yevhenii.kamenskyi@gmail.com>
2026-03-10 14:15:16 -0400

    ci2

--- test/commands/cli/index.test.ts
+++ test/commands/cli/index.test.ts
@@ -4,8 +4,8 @@ import {expect} from 'chai'
 describe('cli', () => {
   it('add and remove', async () => {
     const {stdout} = await runCommand('cli add @razomy/string-case')
-    expect(stdout).to.contain('Installing')
+    expect(stdout).to.contain('installed successfully')
     const {stdout : stdout2} = await runCommand('cli remove @razomy/string-case')
-    expect(stdout2).to.contain('removed')
+    expect(stdout2).to.contain('successfully uninstalled')
   })
 })
